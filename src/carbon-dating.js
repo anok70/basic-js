7bf144a978cbf244a258856195206e09d7023300
anok70 <anok70@mail.ru>
2022-10-15 23:25:00 +0300

    feat: task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let t;
+
+  if (isNaN(sampleActivity) || (Number(sampleActivity) > MODERN_ACTIVITY)  || (Number(sampleActivity) < 0) ||
+  (typeof(sampleActivity) !='string') || (sampleActivity === '') || (sampleActivity === '0')
+  || (sampleActivity === undefined) || sampleActivity === null )
+  
+  {
+    return false;
+  }  
+else  {
+  t = Math.ceil(HALF_LIFE_PERIOD/0.693 * Math.log(MODERN_ACTIVITY / (sampleActivity)));
+  if (t < 30000) return t;
+  else return false;
+}
+
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -12,8 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function encodeLine(/* str */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+  // remove line with error and write your code here }
 
 module.exports = {
   encodeLine
